387501280308ea0596d6e728679515abd8766de6
CoderSadam786 <sh1417537@gmail.com>
2025-08-04 16:15:30 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // KEEP THIS for GitHub Pages
+  // No base for Vercel
+  base: "/",
 
   server: {
     host: "::",
