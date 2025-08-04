8f66df880eede78ba949eb5eb0640a62b772bc27
CoderSadam786 <sh1417537@gmail.com>
2025-08-04 16:46:42 +0500

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
             <div className="relative">
               <div className="w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white shadow-xl animate-fade-in">
                 <Avatar className="w-full h-full">
-                  <AvatarImage   src="C:/sadam/My portfolio/public/m0cou6vcry.jpg"
+                  <AvatarImage   src="/public/m0cou6vcry.jpg"
                     alt="Sadam Hussain"
                     className="object-cover w-full h-full object-top" />
                   <AvatarFallback className="text-2xl"> </AvatarFallback>
